91b290afed9a812b8f927576eb41f101f63bf479
Nicolas Duhamel <nicolas.duhamel@laplateforme.io>
2025-09-18 21:49:39 +0200

    Hide footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         {/* <ThemeSwitcher /> */}
         <div className="min-h-screen">{children}</div>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
